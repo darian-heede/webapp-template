1a10596f78c4716b4a0d3dded275c1f231889345
darian-heede <darian.heede@gmx.de>
2019-06-02 17:32:12 +0200

    Added simple database insert example

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+//mongo --port 27017 -u "app_user" -p "password" --authenticationDatabase "app"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is just a template' });
+	var title = { title: 'This is just a template' };
+	MongoClient.connect(process.env.APP_DB, { useNewUrlParser: true }, (err, db) => {
+		if(err) {
+			title =  { title: err, subtitle: process.env.APP_DB};
+			res.render('index', title);
+			return;
+		}
+		var dbo = db.db('app');
+		dbo.collection('log').insert(
+			{
+				test: "This is a write test"
+			},
+  		(error, result) => {
+		  	if(error) {
+		  		title = { title: error, subtitle: process.env.APP_DB };
+		  		res.render('index', title);
+		  		return;
+		  	}
+	  		title = { title: 'Entry inserted', subtitle: process.env.APP_DB };
+	  		res.render('index', title);
+				db.close();
+				return;
+  		}
+		);
+	});
+  
 });
+
 
 module.exports = router;
